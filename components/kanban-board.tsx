b6166652ffe1907c8ce9a8dddc339cfc7df8bc74
ChaminduD <chamindudulanjana10@gmail.com>
2026-03-11 12:44:36 +0530

    Add new job application form inputs

--- components/kanban-board.tsx
+++ components/kanban-board.tsx
@@ -2,9 +2,10 @@
 
 import { Board, Column } from "@/lib/models/models.types";
 import { Award, Calendar, CheckCircle2, Mic, MoreVertical, Trash2, XCircle } from "lucide-react";
-import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
+import CreateJobApplicationDialog from "./create-job-dialog";
 
 interface KanbanBoardProps {
     board: Board;
@@ -81,6 +82,11 @@ function DroppableColumn({
                     </DropdownMenu>
                 </div>
             </CardHeader>
+
+            <CardContent className="space-y-2 pt-4 bg-gray-50/50 min-h-[400px] rounded-b-lg">
+
+                <CreateJobApplicationDialog columnId={column._id} boardId={boardId} />
+            </CardContent>
         </Card>
     );
 }
